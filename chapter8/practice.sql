13d26734970ea0abd23dd79f7ba52fce2bc2d6a2
大石 琉翔 <ryuto.oishi@rizapgroup.com>
2025-04-16 10:36:32 +0900

    chapter8の練習問題を解いた

--- chapter8/practice.sql
+++ chapter8/practice.sql
@@ -48,3 +48,14 @@ JOIN 社員
 ON 支店.支店長ID = 社員.社員番号
 JOIN (SELECT 勤務地ID, COUNT(*) AS 社員数 FROM 社員 GROUP BY 勤務地ID) AS 社員数
 ON 支店.支店ID = 社員数.勤務地ID;
+
+5
+SELECT 社員.社員番号, 社員.名前, 支店.名前 AS 本人勤務地, 上司支店.名前 AS 上司勤務地
+FROM 社員
+JOIN 社員 AS 上司
+ON 社員.上司ID = 上司.社員番号
+JOIN 支店
+ON 社員.勤務地ID = 支店.支店ID
+JOIN 支店 AS 上司支店
+ON 上司.勤務地ID = 上司支店.支店ID
+WHERE NOT 支店.名前 = 上司支店.名前;
